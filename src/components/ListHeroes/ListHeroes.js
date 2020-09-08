49d630db1ae87f645051f5db707f86f7a8328fe8
Sam Maus <samueldmaus@gmail.com>
2020-09-08 15:28:27 -0500

    heroes stored in heroes reducer and rendered to page

--- src/components/ListHeroes/ListHeroes.js
+++ src/components/ListHeroes/ListHeroes.js
@@ -1,22 +1,52 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import {Grid} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles';
+import {Grid, Card, CardHeader, } from '@material-ui/core'
 
+const styles = theme=> ({
+    card: {
+        margin: 8,
+        textAlign: 'center'
+    },
+    img: {
+        height: 100,
+        width: 100,
+        borderRadius: '50%'
+    },
+    title: {
+        margin: 5
+    }
+})
 
 class ListHeroes extends Component{
+
     componentDidMount(){
-        this.props.dispatch({type:"FETCH_HEROES"})
+        this.props.dispatch({type:"FETCH_HEROES"});
     }
     render(){
+        const {classes} = this.props;
+
         return(
-            <div>
-                {this.props.store.heroes.map(hero => (
-                    <p>{hero.name}</p>
-                ))}
-            </div>
+            <>
+                <div>
+                    <h1 className={classes.title}>Overwatch Heroes</h1>
+                    <br/>
+                
+                </div>
+                <Grid container spacing={1}>
+                    {this.props.store.heroes.map(hero => (
+                        <Grid item xs={3} key={hero.id}>
+                            <Card className={classes.card}>
+                                <CardHeader title={hero.name} subheader={hero.role} />
+                                <img className={classes.img} src={hero.image} alt={hero.name} />
+                            </Card>
+                        </Grid>   
+                    ))}
+                </Grid>
+            </>
         )
     }
 }
 
-export default connect(mapStoreToProps)(ListHeroes);
+export default connect(mapStoreToProps)(withStyles(styles)(ListHeroes));
