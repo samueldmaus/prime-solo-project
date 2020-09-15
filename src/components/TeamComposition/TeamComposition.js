030815f9cd1d9e8834d27a11efe20d93fcf0d729
Sam Maus <samueldmaus@gmail.com>
2020-09-15 15:23:08 -0500

    functionality added to add heroes to team composition

--- src/components/TeamComposition/TeamComposition.js
+++ src/components/TeamComposition/TeamComposition.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import {Grid, Card, Avatar, IconButton} from '@material-ui/core';
+import {Grid, Card, CardHeader, Avatar, IconButton, TextField} from '@material-ui/core';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined'
 
 class TeamComposition extends Component{
@@ -21,9 +21,54 @@ class TeamComposition extends Component{
             support_two: ''
         }
     }
+
+    checkTankTeamComp = (hero) => {
+        if(this.state.comp.tank_one !== '') {
+            this.handleTeamCompChange(hero, 'tank_two')
+        } else{
+            this.handleTeamCompChange(hero, 'tank_one')
+        }
+    };
+
+    checkDPSTeamComp = (hero) => {
+        if(this.state.comp.dps_one !== '') {
+            this.handleTeamCompChange(hero, 'dps_two')
+        } else {
+            this.handleTeamCompChange(hero, 'dps_one')
+        }
+    };
+
+    checkSupportTeamComp = (hero) => {
+        if(this.state.comp.support_one !== '') {
+            this.handleTeamCompChange(hero, 'support_two')
+        } else {
+            this.handleTeamCompChange(hero, 'support_one')
+        }
+    }
+
+    handleTeamCompChange = (hero, property) => {
+        this.setState({
+            ...this.state,
+            comp: {
+                ...this.state.comp,
+                [property]: hero.name
+            }
+        })
+    };
+
+    handleTeamCompName = (event, property) => {
+        this.setState({
+            ...this.state,
+            comp: {
+                ...this.state.comp,
+                [property]: event.target.value
+            }
+        })
+    }
+
     render(){
         return(
-            <Grid >
+            <Grid>
                 <h2>Tank</h2>
                 <Grid>
                 {this.props.store.heroes.map(hero => {
@@ -33,7 +78,7 @@ class TeamComposition extends Component{
                                 <Card>
                                     <p>{hero.name}</p>
                                     <Avatar alt={hero.name} src={hero.image} />
-                                    <IconButton><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
+                                    <IconButton onClick={()=>this.checkTankTeamComp(hero)}><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
                                 </Card>
                             </Grid>
                            
@@ -44,13 +89,13 @@ class TeamComposition extends Component{
                 <br/>
                 <h2>DPS</h2>
                 {this.props.store.heroes.map(hero => {
-                    if(hero.role === 'DPS'){
+                    if(hero.role === 'DPS' && hero.name !== this.state.comp.dps_one && hero.name !== this.state.comp.dps_two){
                         return (
                             <Grid item xs={6} key={hero.id}>
                                 <Card>
                                     <p>{hero.name}</p>
                                     <Avatar alt={hero.name} src={hero.image} />
-                                    <IconButton><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
+                                    <IconButton onClick={()=>this.checkDPSTeamComp(hero)}><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
                                 </Card>
                             </Grid>
                            
@@ -60,18 +105,34 @@ class TeamComposition extends Component{
                 <br/>
                 <h2>Support</h2>
                 {this.props.store.heroes.map(hero => {
-                    if(hero.role === 'Support'){
+                    if(hero.role === 'Support' && hero.name !== this.state.comp.support_one && hero.name !== this.state.comp.support){
                         return (
                             <Grid item xs={6} key={hero.id}>
                                 <Card>
                                     <p>{hero.name}</p>
                                     <Avatar alt={hero.name} src={hero.image} />
-                                    <IconButton><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
+                                    <IconButton onClick={()=>this.checkSupportTeamComp(hero)}><AddCircleOutlineOutlinedIcon></AddCircleOutlineOutlinedIcon></IconButton>
                                 </Card>
                             </Grid>
                         )
                     }
                 })}
+                <br/>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardHeader title="Team Composition" />
+                        <TextField variant="outlined" label="Team Composition Name" value={this.state.comp.name} onChange={(event)=>this.handleTeamCompName(event, 'name')}/>
+                        <h4>{this.state.comp.tank_one}</h4>
+                        <h4>{this.state.comp.tank_two}</h4>
+                        <h4>{this.state.comp.dps_one}</h4>
+                        <h4>{this.state.comp.dps_two}</h4>
+                        <h4>{this.state.comp.support_one}</h4>
+                        <h4>{this.state.comp.support_two}</h4>
+                    </Card>
+                    
+                </Grid>
+               
+                
             </Grid>
         )
     }
